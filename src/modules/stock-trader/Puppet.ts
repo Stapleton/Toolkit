5cf2b61a9ac3cc0d8a322e36941179a8f4e4672f
Taylor Stapleton <stapletontaylor@hotmail.com>
2022-09-27 03:18:42 -0600

    chippin away

--- src/modules/stock-trader/Puppet.ts
+++ src/modules/stock-trader/Puppet.ts
@@ -3,6 +3,7 @@
 import Puppeteer from "puppeteer";
 import { Signale } from "signale";
 import Config from "./Config";
+import Sleep from "../../core/lib/Sleep";
 
 const name = [`Puppet`, `#${process.argv0}`];
 let Logger = new Signale({
@@ -37,13 +38,26 @@ function stopTime(): { label: string; span?: number } {
 	return time;
 }
 
-function doThing(Page: Puppeteer.Page) {
-	Logger.disable();
-	Logger.info(Page); // Use `Page` variable so TSC stops being angy
-	Logger.enable();
+/*function openOrders(Page: Puppeteer.Page) {
+	let touch = Page.touchscreen;
+	touch.tap(640, 810);
+}*/
 
-	Logger.error(`Not Yet Implemented.`);
-	// Traverse through each necessary step for a Puppet to make a stock order
+async function getCookiesForAutoLogin(Page: Puppeteer.Page) {
+	let touch = Page.touchscreen;
+	await touch.tap(662, 472);
+	Sleep(500);
+	await touch.tap(430, 330);
+	// ! Google Signin opens a new tab, i have no clue how to handle this at 3am
+	// ! The text box is autoselected tho so thats nice but idk if the page object will change or
+	// ! if I have to start passing through the entire array of Pages
+	// * 20% of the progress takes 80% of the effort. You can get through this
+	// * Just dedicate yourself. This is one of the last times you will have to work in your life.
+	// * Git 'er done bud, you can do it you unstoppable chad!
+
+	// ? Could I make a function that when given a dom element will calculate and return
+	// ? its pixel placement on the page in x and y
+	//openOrders(Page);
 }
 
 async function Puppet() {
@@ -54,20 +68,21 @@ async function Puppet() {
 	let Pages = await Browser.pages(),
 		Page = Pages[0];
 
-	Logger.info(`Changing Viewport Size to ${Config.Puppet.View.width}x${Config.Puppet.View.height}...`);
+	Logger.info(`Changing Viewport Size to 1000x850...`);
 	await Page.setViewport({
-		width: Config.Puppet.View.width,
-		height: Config.Puppet.View.height,
+		width: 1000,
+		height: 850,
 	});
 
 	Logger.pending(`Heading to ${Config.Puppet.initLink}`);
 	await Page.goto(Config.Puppet.initLink);
 
 	Logger.await(`Doing thing.`);
-	doThing(Page);
+	getCookiesForAutoLogin(Page);
+	//openOrders(Page);
 
 	Logger.complete(`Spawned. Took ${stopTime().span}ms. Closing Puppet...`);
-	Browser.close();
+	//Browser.close();
 }
 
 export default Puppet;
